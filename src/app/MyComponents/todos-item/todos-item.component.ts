f8ef721d56e7459fab7803e3d5ef340d9539a028
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-08-10 15:52:11 +0500

    Adding input in todos-item ts file

--- src/app/MyComponents/todos-item/todos-item.component.ts
+++ src/app/MyComponents/todos-item/todos-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todos-item',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos-item.component.css']
 })
 export class TodosItemComponent implements OnInit {
-
+  // Adding input (as in react when we pass a props same as it in angular we have to also define it in input)
+  @Input() item : Todo;
   constructor() { }
 
   ngOnInit(): void {
